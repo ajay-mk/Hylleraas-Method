c3549540103ca90ff005e0763a47585e3b8db163
Ajay <ajaymk22@vt.edu>
2023-10-18 14:20:14 -0400

    Add timing and verbose mode to Hylleraas calculation
    Added the <chrono> library to enable timing of the main calculation loop. An optional "verbose" mode was also added, which displays the time taken for each iteration if enabled, to provide more information on the program's performance.

--- main.cpp
+++ main.cpp
@@ -5,11 +5,14 @@
 // Hylleraas Method Implementation Project for CHEM6664, Spring 23
 
 #include "helper_functions.h"
+#include <chrono>
 #include <iomanip>
 #include <iostream>
 
 int main(int argc, char *argv[]) {
   std::cout << std::setprecision(16);
+  constexpr bool verbose = true;
+
   std::cout << "Hylleraas Method\n" << std::endl;
 
   BasisFn bs_single = {{{0, 0, 0}, {1.6875, 1.6875, 0.0}}};
@@ -42,16 +45,34 @@ int main(int argc, char *argv[]) {
 
   // Calculation of ground state energy of Helium atom
   std::cout << "\nCalculation of ground state energy of Helium atom\n";
-  // vary N from 0 to 15
+  // vary N from 0 to 12
 
-  std::cout << "N\tEnergy (Eh)" << std::endl;
   for (auto n = 0; n <= 12; ++n) {
+    auto start = std::chrono::high_resolution_clock::now();
+
     // construct basis with alpha = beta = 1.8, gamma = 0;
     // set Z = 2 for Helium atom
-
     auto result = do_hylleraas(n, 1.8, 0.0, 2.0);
 
-    std::cout << n << "\t" << result.evals(0) << std::endl;
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration =
+        std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+    // display results
+    if (verbose) {
+      if (n == 0) {
+        std::cout << "N \t Energy (Eh) \t Time (ms)"
+                  << std::endl; // print header
+      }
+      std::cout << n << "\t" << result.evals(0) << "\t" << duration.count()
+                << std::endl;
+    } else {
+      if (n == 0) {
+        std::cout << "N \t Energy (Eh) \t Time taken (milliseconds)"
+                  << std::endl; // print header
+      }
+      std::cout << n << "\t" << result.evals(0) << std::endl;
+    }
   }
 
   return 0;
